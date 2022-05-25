ee627e5b4a9a924dfd38ff0ba39b05f7475d07ac
Ramona Rotaru <ramona.rotaru232@gmail.com>
2022-05-25 17:41:54 +0200

    k commit

--- components/Modal.js
+++ components/Modal.js
@@ -30,7 +30,7 @@ export default function Modal({ handleClose, type }) {
 			{type === "dropIn" && (
 				<motion.div
 					onClick={(e) => e.stopPropagation()}
-					className="rounded-sm flex flex-col justify-center bgTheme textTheme w-full max-w-lg mx-4 -mt-20 z-50 lg:mt-4 xl:mt-10"
+					className="rounded-sm flex flex-col justify-center bgTheme textTheme w-full max-w-lg mx-4 -mt-24 z-50 lg:mt-4 xl:mt-10"
 					variants={dropIn}
 					initial="hidden"
 					animate="visible"
